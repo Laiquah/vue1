d7b5a787af8e84fc968a9fd0753c4b2ae528c129
Laiquah <cotielaiquah@gmail.com>
2023-06-27 16:36:45 +0200

    add

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="nav">
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a> 
+                </li>
+                <li>
+                    <a href="#products">Products</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col" v-for="item in data" :key="item.ID">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="item.image" class="card-img-top" :alt="item.title">
+                        <div class="card-body">
+                            <h1>{{item.tile}}</h1>
+                            <h4>{{item.price}}</h4>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                data: []
+            }
+        },
+        methods: {
+            async fetchData() {
+                const res = await fetch("https://codebeans2022.github.io/JSON-Server/data.json");
+                let parsedData = await res.json();
+                this.data = parsedData.products;
+            }
+        },
+        mounted() {
+            this.fetchData();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
